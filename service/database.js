10bafd0872bb035a9672d66711b1286c830fdcc7
Connor Robb <connor.m.robb33@gmail.com>
2025-04-12 18:10:37 -0600

    format service

--- service/database.js
+++ service/database.js
@@ -1,36 +1,38 @@
-const { MongoClient } = require('mongodb');
-const config = require('./dbConfig.json');
+const { MongoClient } = require("mongodb");
+const config = require("./dbConfig.json");
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
-const db = client.db('simon');
-const userCollection = db.collection('user');
+const db = client.db("simon");
+const userCollection = db.collection("user");
 
 // This will asynchronously test the connection and exit the process if it fails
 (async function testConnection() {
-  try {
-    await db.command({ ping: 1 });
-    console.log(`Connect to database`);
-  } catch (ex) {
-    console.log(`Unable to connect to database with ${url} because ${ex.message}`);
-    process.exit(1);
-  }
+    try {
+        await db.command({ ping: 1 });
+        console.log(`Connect to database`);
+    } catch (ex) {
+        console.log(
+            `Unable to connect to database with ${url} because ${ex.message}`,
+        );
+        process.exit(1);
+    }
 })();
 
 function getUser(username) {
-  return userCollection.findOne({ username: username });
+    return userCollection.findOne({ username: username });
 }
 
 function getUserByToken(token) {
-  return userCollection.findOne({ token: token });
+    return userCollection.findOne({ token: token });
 }
 
 async function addUser(user) {
-  await userCollection.insertOne(user);
+    await userCollection.insertOne(user);
 }
 
 async function updateUser(user) {
-  await userCollection.updateOne({ username: user.username}, { $set: user });
+    await userCollection.updateOne({ username: user.username }, { $set: user });
 }
 
 //function getHighScores() {
@@ -44,8 +46,8 @@ async function updateUser(user) {
 //}
 
 module.exports = {
-  getUser,
-  getUserByToken,
-  addUser,
-  updateUser,
+    getUser,
+    getUserByToken,
+    addUser,
+    updateUser,
 };
--- service/index.js
+++ service/index.js
@@ -1,128 +1,130 @@
-const cookieParser = require('cookie-parser');
-const bcrypt = require('bcryptjs');
-const express = require('express');
-const logger = require('express-requests-logger')
-const uuid = require('uuid');
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcryptjs");
+const express = require("express");
+const logger = require("express-requests-logger");
+const uuid = require("uuid");
 const app = express();
-const DB = require('./database.js');
+const DB = require("./database.js");
 
-const authCookieName = 'token';
+const authCookieName = "token";
 
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(logger({
-    request: {
-        excludeHeaders: [`*`],
-        excludeBody: [`*`]
-    },
-    response: {
-        excludeHeaders: [`*`],
-        excludeBody: [`*`]
-    }
-}));
+app.use(
+    logger({
+        request: {
+            excludeHeaders: [`*`],
+            excludeBody: [`*`],
+        },
+        response: {
+            excludeHeaders: [`*`],
+            excludeBody: [`*`],
+        },
+    }),
+);
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 const apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
-apiRouter.post('/auth/create', async (req, res) => {
-  if (await findUser('username', req.body.username)) {
-    res.status(409).send({ msg: 'Existing user' });
-  } else {
-    const user = await createUser(req.body.username, req.body.password);
+apiRouter.post("/auth/create", async (req, res) => {
+    if (await findUser("username", req.body.username)) {
+        res.status(409).send({ msg: "Existing user" });
+    } else {
+        const user = await createUser(req.body.username, req.body.password);
 
-    setAuthCookie(res, user.token);
-    res.send({ username: user.username});
-  }
-});
-
-apiRouter.post('/auth/login', async (req, res) => {
-  console.log("RECEIVED REQUEST")
-  const user = await findUser('username', req.body.username);
-  if (user) {
-    if (await bcrypt.compare(req.body.password, user.password)) {
-      user.token = uuid.v4();
-      await DB.updateUser(user);
-      setAuthCookie(res, user.token);
-      res.send({ username: user.username});
-      return;
+        setAuthCookie(res, user.token);
+        res.send({ username: user.username });
     }
-  }
-  res.status(401).send({ msg: 'Unauthorized' });
 });
 
-apiRouter.delete('/auth/logout', async (req, res) => {
-  const user = await findUser('token', req.cookies[authCookieName]);
-  if (user) {
-    delete user.token;
-    DB.updateUser(user);
-  }
-  res.clearCookie(authCookieName);
-  res.status(204).end();
+apiRouter.post("/auth/login", async (req, res) => {
+    console.log("RECEIVED REQUEST");
+    const user = await findUser("username", req.body.username);
+    if (user) {
+        if (await bcrypt.compare(req.body.password, user.password)) {
+            user.token = uuid.v4();
+            await DB.updateUser(user);
+            setAuthCookie(res, user.token);
+            res.send({ username: user.username });
+            return;
+        }
+    }
+    res.status(401).send({ msg: "Unauthorized" });
+});
+
+apiRouter.delete("/auth/logout", async (req, res) => {
+    const user = await findUser("token", req.cookies[authCookieName]);
+    if (user) {
+        delete user.token;
+        DB.updateUser(user);
+    }
+    res.clearCookie(authCookieName);
+    res.status(204).end();
 });
 
 const userAuthMw = async (req, res, next) => {
-  const user = await findUser('token', req.cookies[authCookieName]);
-  if (user) {
-    next();
-  } else {
-    res.status(401).send({ msg: 'Unauthorized' });
-  }
+    const user = await findUser("token", req.cookies[authCookieName]);
+    if (user) {
+        next();
+    } else {
+        res.status(401).send({ msg: "Unauthorized" });
+    }
 };
 const anonAuthMw = async (req, res, next) => {
-  const user = await findUser('token', req.cookies[authCookieName]);
-  if (user) {
-    next();
-  } else {
-    res.status(401).send({ msg: 'Unauthorized' });
-  }
+    const user = await findUser("token", req.cookies[authCookieName]);
+    if (user) {
+        next();
+    } else {
+        res.status(401).send({ msg: "Unauthorized" });
+    }
 };
 
 // Error Handler
 app.use(function (err, req, res, next) {
-  res.status(500).send({ type: err.name, message: err.message });
+    res.status(500).send({ type: err.name, message: err.message });
 });
 
 // Return the application's default page if the path is unknown
 app.use((_req, res) => {
-  res.sendFile('index.html', { root: 'public' });
+    res.sendFile("index.html", { root: "public" });
 });
 
 async function createUser(username, password) {
-  const passwordHash = await bcrypt.hash(password, 10);
+    const passwordHash = await bcrypt.hash(password, 10);
 
-  const user = {
-    username: username,
-    password: passwordHash,
-    token: uuid.v4(),
-  };
-  await DB.addUser(user);
+    const user = {
+        username: username,
+        password: passwordHash,
+        token: uuid.v4(),
+    };
+    await DB.addUser(user);
 
-  return user;
+    return user;
 }
 
 async function findUser(field, value) {
-  if (!value) return null;
+    if (!value) return null;
 
-  console.log(value)
-  if (field === 'token') {
-    return DB.getUserByToken(value);
-  }
-  return DB.getUser(value);
+    console.log(value);
+    if (field === "token") {
+        return DB.getUserByToken(value);
+    }
+    return DB.getUser(value);
 }
 
 // setAuthCookie in the HTTP response
 function setAuthCookie(res, authToken) {
-  res.cookie(authCookieName, authToken, {
-    secure: true,
-    httpOnly: true,
-    sameSite: 'strict',
-  });
+    res.cookie(authCookieName, authToken, {
+        secure: true,
+        httpOnly: true,
+        sameSite: "strict",
+    });
 }
 
 const httpService = app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+    console.log(`Listening on port ${port}`);
 });
